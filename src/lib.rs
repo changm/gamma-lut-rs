b510378ca501651415fe8f73bc0cf8005ae6e7f1
Lee Salzman <lsalzman@gmail.com>
2017-10-28 00:54:46 -0400

    make fields of Color public

--- src/lib.rs
+++ src/lib.rs
@@ -105,10 +105,10 @@ pub const LUM_BITS: u8 = 3;
 
 #[derive(Copy, Clone)]
 pub struct Color {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
 }
 
 impl Color {
